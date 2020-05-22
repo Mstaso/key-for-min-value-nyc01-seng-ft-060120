11f0257dba14365a89d132a3a9c1be3b4fc62bf0
malcolm staso <mstaso1@gmail.com>
2020-05-22 15:15:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ name_hash.each do |key, value|
   if current_minimum_val == nil
     current_minimum_val = nil
     current_key_assoc_w_min_val = key
-  elsif
+  elsif current_key_assoc_w_min_val
   
  end
 end
